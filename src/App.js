3004cd1a1ce7f9f9c234954cd572aa92f76f0e09
scientistmind <luk.ku@hotmail.com>
2021-06-02 12:38:01 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import {useState} from 'react';
+import "./App.css";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [state, setstate] = useState([])
+  return(
+    <div>
+      
+      <h4>udeamy Free Courses</h4>
+
+      <button onClick={()=>{
+       axios.get('https://sumanjay.vercel.app/udemy').then((response)=>{
+         console.log(response.data)
+         setstate(response.data)
+       })
+      }}>Click Me To Fetch Data</button>
+      {state.map((obj)=>{
+        return(
+          <div>
+            <h1>{obj.title}</h1>
+
+           <img src={obj.image} alt={obj.title} height={500} width={500}  />
+           <p>{obj.description}</p>
+           <a href={obj.link}><button> Click To Enroll</button></a>
+          </div>
+        )
+      })}
     </div>
   );
 }
